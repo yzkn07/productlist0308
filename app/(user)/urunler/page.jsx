fae15703236c3e9497b7977d77e5c0a2af33090d
yzkn07 <mehmetali.1992@hotmail.com>
2024-08-29 23:40:42 +0300

    form improved

--- app/(user)/urunler/page.jsx
+++ app/(user)/urunler/page.jsx
@@ -5,9 +5,12 @@ export default async  function ProductsPage() {
 
     return (
         <>
-            <form action="">
-                <input type="text" name="" id="" placeholder="ürün ara"
-                className="border-zinc-300 focus:border-zinc-700 border-2 ms-2 py-1 px-2 rounded-md"/>
+            <form className="mb-2 flex flex-col mx-2">
+                <input type="text" name="productName" id="" placeholder="ürün ara"
+                className="border-zinc-300 focus:border-zinc-700 border-2 py-1 px-2 rounded-md mb-2"/>
+                <input type="text" name="productCategory" id="" placeholder="kategori ara"
+                className="border-zinc-300 focus:border-zinc-700 border-2 py-1 px-2 rounded-md mb-2"/>
+                <button className="bg-gray-400 py-1 px-4 rounded-md text-white text-lg">ara</button>
             </form>
 
             <div className="grid grid-cols-12">
