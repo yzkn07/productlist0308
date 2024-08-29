a4aa884db1cae2e9d57d2ad80ddeacf723b8b84c
yzkn07 <mehmetali.1992@hotmail.com>
2024-08-29 18:29:29 +0300

    ürünler sayfası grid düzenlendi

--- app/(user)/urunler/[id]/page.jsx
+++ app/(user)/urunler/[id]/page.jsx
@@ -1,8 +1,13 @@
 import ProductCard from "@/components/partials/user/ProductCard"
+import { notFound } from "next/navigation"
 
 export default async function ProductDetailPage({ params }) {
     const { id } = params
-    const product = await fetch(`https://dummyjson.com/products/${id}`).then(r => r.json())
+    const request = await fetch(`https://dummyjson.com/products/${id}`)    
+    if (!request.ok) {
+        return notFound()
+    }
+    const product = await request.json()
     return (
         <>
             <ProductCard product={product}/>
--- components/partials/user/ProductCard.jsx
+++ components/partials/user/ProductCard.jsx
@@ -11,10 +11,10 @@ export default function ProductCard({ product }) {
         <div onClick={() => {
             router.push(`/urunler/${product.id}`)
             }} 
-            className="col-span-12 sm:col-span-6 md:col-span-4  xl:col-span-3 mb-3 mt-5 mx-auto text-white bg-gradient-to-t from-cyan-500 to-blue-500 py-3 px-2 w-11/12 rounded-md  shadow-xl flex gap-5 flex-col text-center justify-center items-center cursor-pointer">
-            <h1 className=" bg-slate-300/30 rounded-md py-2 px-4 font-bold">{product.title}</h1>
+            className="col-span-6 sm:col-span-6 md:col-span-4  xl:col-span-3 mb-3 mt-2 mx-auto text-white bg-gradient-to-t from-cyan-500 to-blue-500 py-3 px-2 w-11/12 rounded-md  shadow-xl flex gap-5 flex-col text-center justify-center items-center cursor-pointer">
+            <h1 className=" bg-slate-300/30 rounded-md py-2 px-4 font-bold text-xs sm:text-lg xl:text-xl">{product.title}</h1>
             <Image src={product.thumbnail} width={200} height={200} alt={product.category} priority/>
-            <div className="bg-slate-100/30 rounded-md py-2 px-4 text-white font-thin hover:text-white hover:border-indigo-500 hover:border-2">
+            <div className="bg-slate-100/30 rounded-md py-2 px-4 text-white font-light hover:text-white hover:border-indigo-500 hover:border-2">
             <span className="me-2">$</span>{product.price}
             </div>
         </div>
