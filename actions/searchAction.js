2e42846640fcc3cbe036ec9a9ba8d410c22a921d
yzkn07 <mehmetali.1992@hotmail.com>
2024-09-02 18:04:21 +0300

    added min price filter

--- actions/searchAction.js
+++ actions/searchAction.js
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 export async function ProductFilterAction(formData) {
     const productName = formData.get("productName")
     // const productCategory = formData.get("productCategory")
+    const minPrice = formData.get("minPrice")
     
     
     const query = [];
@@ -14,6 +15,9 @@ export async function ProductFilterAction(formData) {
     // if(productCategory){
     //     query.push(`productCategory=${productCategory}`)
     // }
+    if(minPrice){
+        query.push(`minPrice=${minPrice}`)
+    }
     
     return redirect(`/urunler?${query.join("&")}`)
     
--- app/(user)/urunler/page.jsx
+++ app/(user)/urunler/page.jsx
@@ -2,8 +2,14 @@ import { ProductFilterAction } from "@/actions/searchAction";
 import ProductCard from "@/components/partials/user/ProductCard";
 
 export default async  function ProductsPage({ searchParams }) {
-    const { products: Products } = await fetch("https://dummyjson.com/products").then(r => r.json())
-    console.log(searchParams);
+    let { products: Products } = await fetch("https://dummyjson.com/products").then(r => r.json())
+    if( searchParams.productName){
+        Products = Products.filter(p => 
+            p.title.toLocaleLowerCase().includes(searchParams.productName.toLocaleLowerCase()))
+    } 
+    if( searchParams.minPrice){
+        Products = Products.filter(p => p.price >= searchParams.minPrice)
+    }
     
 
     return (
@@ -14,6 +20,9 @@ export default async  function ProductsPage({ searchParams }) {
                
                 {/* <input type="text" name="productCategory" id="" placeholder="kategori ara"
                 className="border-zinc-300 focus:border-zinc-700 border-2 py-1 px-2 rounded-md mb-2"/> */}
+
+                <input type="number" name="minPrice" id="" placeholder="en düşük fiyat gir"
+                className="border-zinc-300 focus:border-zinc-700 border-2 py-1 px-2 rounded-md mb-2"/>
                
                 <button className="bg-gray-400 py-1 px-4 rounded-md text-white text-lg">ara</button>
             </form>
@@ -27,12 +36,15 @@ export default async  function ProductsPage({ searchParams }) {
                     {/* bütün ürünleri listeliyor  */}
                 
                     {/* searchParams varsa productName geliyor, p.title ile eşleşenleri listele */}
-                {searchParams.productName ? Products.filter(p => p.title.toLocaleLowerCase().includes(searchParams.productName.toLocaleLowerCase())).map(p => (
+                {/* {searchParams.productName ? Products.filter(p => p.title.toLocaleLowerCase().includes(searchParams.productName.toLocaleLowerCase())).map(p => (
                     <ProductCard key={p.id} product={p}/>
                 )) : Products.map(p => (
                     <ProductCard key={p.id} product={p}/>
                     ))
-                }   
+                }    */}
+                {Products.map(p => (
+                    <ProductCard key={p.id} product={p}/>)) 
+                    }
             </div>
         </>
     )
